6b523026b0b6907e32dd4f60f9adb040c9fa57b2
romankornienko <Romansky199414>
2022-04-15 14:55:59 +0300

    review fixes3

--- src/features/Main.tsx
+++ src/features/Main.tsx
@@ -1,6 +1,6 @@
 import Header from "./Header";
 import Cases from "./Cases/Cases";
-import Footer from "./Footer/Footer";
+import Footer from "../components/Footer/Footer";
 import ScrollToTop from "react-scroll-to-top";
 import { blue } from "../colors";
 import QuestionForm from "./QuestionForm/QuestionForm";
